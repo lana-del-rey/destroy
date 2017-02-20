a550d40e9534de5cd8d064855c1a25ec45500091
lana-del-rey <iskandar@inbox.ru>
2017-02-20 03:47:30 +0300

    hello world 3

--- DestroyWorld/ViewController.swift
+++ DestroyWorld/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
     
-        print("hello world")
+        print("Hello World!")
     }
 
     override func didReceiveMemoryWarning() {
